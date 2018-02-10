ea33911c8cc53be25adbc463e19b1c383b19de78
bkang61wk <31371610+bkang61wk@users.noreply.github.com>
2018-02-10 17:19:22 -0500

    Add unit test for find
    The test is just counting number of files in the directory recursively. Add more conditions later.

---
+++ DuplicateFinder/DuplicateFinderUnitTests/CoreUnitTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+using DuplicateFinder;
+using System.Linq;
+
+namespace DuplicateFinderUnitTests
+{
+    [TestClass]
+    public class CoreUnitTests
+    {
+        private List<string> directories = new List<string>
+        {
+            "root", "root\\dir1", "root\\dir2",
+            "root\\dir1\\dir1"
+        };
+
+        private List<string> files = new List<string>
+        {
+            "file1", "file2", "file3", "file4"
+        };
+
+        private List<string> fileContents = new List<string>
+        {
+            "This is duplicate file.", "This is not a duplicate file."
+        };
+
+        [TestMethod]
+        public void FindTests()
+        {
+            List<FileInfo> files = Core.Find(new List<DirectoryInfo> { new DirectoryInfo(directories[0]) }).ToList();
+
+            // For now - Add more conditions
+            Assert.IsTrue(files.Count == 16);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            foreach(string directory in directories)
+            {
+                Directory.CreateDirectory(directory);
+
+                foreach(string file in files)
+                {
+                    using (StreamWriter fs = File.CreateText(Path.Combine(directory, file)))
+                    {
+                        // file1-3 are duplicates
+                        if(file.Equals(files[3]))
+                        {
+                            fs.WriteLine(fileContents[1]); // not a duplicate
+                        }
+                        else
+                        {
+                            fs.WriteLine(fileContents[0]); // duplicate
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            Directory.Delete(directories[0], true);
+        }
+    }
+}
---
+++ DuplicateFinder/DuplicateFinderUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DuplicateFinderUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DuplicateFinderUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd0b20d2-f306-4658-b69b-c3ca18a3ffd9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
