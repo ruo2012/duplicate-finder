2d21fcdbcb605b8884e9b86345712d7d12300a4e
bkang61wk <31371610+bkang61wk@users.noreply.github.com>
2018-02-10 16:37:59 -0500

    Add DuplicateGroup class

---
+++ DuplicateFinder/DuplicateFinder/DuplicateGroup.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DuplicateFinder
+{
+    /// <summary>
+    /// A class representing a group of duplicate files
+    /// </summary>
+    public class DuplicateGroup
+    {
+        /// <summary>
+        /// MD5 hash of this duplicate group
+        /// </summary>
+        public string Hash { get; private set; }
+
+        /// <summary>
+        /// File size of this duplicate group
+        /// </summary>
+        public long FileSize { get; private set; }
+
+        /// <summary>
+        /// File paths of this duplicate group
+        /// </summary>
+        public IEnumerable<FileInfo> FilePaths
+        {
+            get { return Files.AsReadOnly(); }
+        }
+
+        /// <summary>
+        /// Number of duplicate files in this group
+        /// </summary>
+        public int Count => Files.Count;
+
+        private List<FileInfo> Files;
+
+        public DuplicateGroup(string hash, FileInfo file)
+        {
+            Hash = hash;
+            FileSize = file.Length;
+            Files.Add(file);
+        }
+
+        /// <summary>
+        /// Add a file path to this duplicate group
+        /// </summary>
+        /// <param name="filePath">Duplicate file path</param>
+        public void Add(FileInfo file)
+        {
+            Files.Add(file);
+        }
+    }
+}
