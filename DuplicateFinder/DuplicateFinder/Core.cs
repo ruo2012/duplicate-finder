68952d76ed4c146bfadee289b4f7ba422317ead8
bkang61wk <31371610+bkang61wk@users.noreply.github.com>
2018-02-10 15:46:29 -0500

    Add Core and Extension classes
    Core class will contain processing logics and Extension class will contain any useful extension methods used in Core class.

---
+++ DuplicateFinder/DuplicateFinder/Core.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DuplicateFinder
+{
+    /// <summary>
+    /// A class responsible for core functions
+    /// </summary>
+    public class Core
+    {
+        /// <summary>
+        /// Extension for any file
+        /// </summary>
+        public const string FileExtensionAny = "*.*";
+
+        /// <summary>
+        /// Find all files in given directories
+        /// </summary>
+        /// <param name="directories">Target directories</param>
+        /// <param name="extensions">Target file extensions. Searches for all extensions if this is null or empty. Default is null.</param>
+        /// <param name="recursive">If true, searches recursively. Default is true.</param>
+        /// <returns>FileInfo IEnumerable</returns>
+        public static IEnumerable<FileInfo> Find(IEnumerable<string> directories, IEnumerable<string> extensions = null, bool recursive = true)
+        {
+            if(directories.IsNullOrEmpty())
+            {
+                throw new ArgumentException("Directories cannot be null or empty.");
+            }
+
+            if(extensions.IsNullOrEmpty())
+            {
+                extensions = new List<string> { FileExtensionAny };
+            }
+
+            IEnumerable<FileInfo> fileInfos = null;
+            SearchOption searchOption = recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+
+            foreach (string directory in directories)
+            {
+                fileInfos = fileInfos.IsNullOrEmpty() ? 
+                    FindHelper(directory, extensions, recursive) : 
+                    fileInfos.Union(FindHelper(directory, extensions, recursive));
+            }
+
+            return fileInfos;
+        }
+
+        /// <summary>
+        /// Helper method for the public method <see cref="Find(IEnumerable{string}, IEnumerable{string}, bool)"/>.
+        /// </summary>
+        /// <param name="directory">Target directory</param>
+        /// <param name="extensions">Target extensions</param>
+        /// <param name="recursive">If true, searches the directory recursively</param>
+        /// <returns>FileInfo IEnumerable</returns>
+        private static IEnumerable<FileInfo> FindHelper(string directory, IEnumerable<string> extensions, bool recursive)
+        {
+            List<FileInfo> fileInfos = new List<FileInfo>();
+            SearchOption searchOption = recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+
+            foreach(string extension in extensions)
+            {
+                foreach (string filePath in Directory.EnumerateFiles(directory, extension, searchOption))
+                {
+                    fileInfos.Add(new FileInfo(filePath));
+                }
+            }
+
+            return fileInfos;
+        }
+    }
+}
---
+++ DuplicateFinder/DuplicateFinder/Extension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateFinder
+{
+    /// <summary>
+    /// A class responsible for extension methods used in Core class.
+    /// </summary>
+    public static class Extension
+    {
+        /// <summary>
+        /// Determine whether IEnumerable is null or empty.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty<T> (this IEnumerable<T> source)
+        {
+            return source == null || !source.Any();
+        }
+    }
+}
